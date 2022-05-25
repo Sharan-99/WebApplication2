813f75349195cc464fe94c1a36e44a1eabe3be86
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:36:10 +0530

    first commit

---
+++ WebApplication2/Controllers/Ex3Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class Ex3Controller : Controller
+    {
+        List<ShoppingCart> l1 = new List<ShoppingCart>() {new ShoppingCart("CA1","Shoe",3000,"Sport"),
+                                                          new ShoppingCart("CA2", "Shirt", 1500, "Men's Wear"),
+                                                          new ShoppingCart("CA3","Watch",4000,"Accessories"),
+                                                          new ShoppingCart("CA4","Samsung",15000,"Mobiles")};
+        // GET: Ex3
+        public ActionResult Cart(int id)
+        {
+            return View("Cart",l1[id-1]);
+        }
+
+        [HttpPost]
+        public ActionResult Cart(ShoppingCart shoppingCart)
+        {
+            return View(shoppingCart);
+        }
+    }
+}
---
+++ WebApplication2/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ShoppingCart
+    {
+        public String CartId { get; set; }
+        public String ItemName { get; set; }
+        public int Price { get; set; }
+        public string ItemType { get; set; }
+
+        public ShoppingCart(String cartId, String itemName, int price, String itemType)
+        {
+            this.ItemName = itemName;
+            this.CartId = cartId;
+            this.Price = price;
+            this.ItemType = itemType;
+        }
+    }
+}
